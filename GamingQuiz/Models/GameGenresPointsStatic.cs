264a05f1ddf32deb2be536dfd6f37e8b1b2ec425
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-04 20:04:14 +0200

    Added GameGenresPointsStatic

---
+++ GamingQuiz/Models/GameGenresPointsStatic.cs
@@ -0,0 +1,9 @@
+﻿namespace GamingQuiz.Models
+{
+    public static class GameGenresPointsStatic
+    {
+        public static int Strategy { get; set; }
+        public static int FirstPersonShooters { get; set; }
+        public static int Simulators { get; set; }
+    }
+}
