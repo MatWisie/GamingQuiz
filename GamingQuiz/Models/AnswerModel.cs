f2dfe21fe3f4ca700de217b91c28e4a751d1082b
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-04 20:03:56 +0200

    Created AnswerModel

---
+++ GamingQuiz/Models/AnswerModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace GamingQuiz.Models
+{
+    public class AnswerModel
+    {
+        public AnswerModel(Dictionary<GenresEnum, int> PointsToCategories)
+        {
+            this.PointsToCategories = PointsToCategories;
+        }
+        public Dictionary<GenresEnum, int> PointsToCategories;
+    }
+}
