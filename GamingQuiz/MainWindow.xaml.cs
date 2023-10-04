3bae8896146b1f0e9052b7aebdf139427c62d16c
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-04 20:07:16 +0200

    Created example Question and answers

--- GamingQuiz/MainWindow.xaml.cs
+++ GamingQuiz/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using GamingQuiz.Views;
+﻿using GamingQuiz.Models;
+using GamingQuiz.Views;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace GamingQuiz
@@ -8,10 +10,23 @@ namespace GamingQuiz
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<QuestionModel> questions = new List<QuestionModel>()
+        {
+            new QuestionModel("Is cereal a soup", "", new AnswerModel[]
+            {
+                new AnswerModel(new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 10 },
+                    {GenresEnum.RPG, 5 },
+                    {GenresEnum.Simulators, 10 },
+                    {GenresEnum.Strategy, 0 }
+                }),
+            }),
+        };
         public MainWindow()
         {
             InitializeComponent();
-            WindowContent.Content = new Question("im a question!!!");
+            WindowContent.Content = new Question(questions[0]);
         }
     }
 }
