47e7a4d9ee7db545cb26fdc5d8ea246979eb33a6
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-04 20:04:26 +0200

    Added QuestionModel

---
+++ GamingQuiz/Models/QuestionModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GamingQuiz.Models
+{
+    public class QuestionModel
+    {
+        public QuestionModel(string questionText, string questionImagePath, AnswerModel[] answers)
+        {
+            this.questionText = questionText;
+            this.questionImagePath = questionImagePath;
+            this.answers = answers;
+        }
+        public string questionText { get; set; }
+        public string questionImagePath { get; set; }
+        public AnswerModel[] answers { get; set; }
+    }
+}
