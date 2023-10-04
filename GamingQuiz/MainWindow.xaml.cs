e9706753d0be08f3f40c34cab130a70861013a64
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-04 11:54:00 +0200

    Add project files.

---
+++ GamingQuiz/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using GamingQuiz.Views;
+using System.Windows;
+
+namespace GamingQuiz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowContent.Content = new Question("im a question!!!");
+        }
+    }
+}
---
+++ GamingQuiz/Views/Question.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace GamingQuiz.Views
+{
+    /// <summary>
+    /// Interaction logic for Question.xaml
+    /// </summary>
+    public partial class Question : UserControl
+    {
+        public Question(string text)
+        {
+            InitializeComponent();
+            LabelInContent.Content = text;
+        }
+    }
+}
