e9c816a1be13c16a4de3a3d306fdb85873b8f459
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-08 13:12:41 +0200

    Implemented new AnswerModel Dictionary in MainWindow

--- GamingQuiz/MainWindow.xaml.cs
+++ GamingQuiz/MainWindow.xaml.cs
@@ -14,12 +14,64 @@ namespace GamingQuiz
         {
             new QuestionModel("Is cereal a soup", "", new AnswerModel[]
             {
-                new AnswerModel(new Dictionary<GenresEnum,int>
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<GenresEnum,int>
                 {
                     {GenresEnum.FirstPersonShooters, 10 },
                     {GenresEnum.RPG, 5 },
                     {GenresEnum.Simulators, 10 },
                     {GenresEnum.Strategy, 0 }
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 20 },
+                    {GenresEnum.RPG, 10 },
+                    {GenresEnum.Simulators, 15 },
+                    {GenresEnum.Strategy, 0 }
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 50 },
+                    {GenresEnum.RPG, 60 },
+                    {GenresEnum.Simulators, 5 },
+                    {GenresEnum.Strategy, 1 }
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 20 },
+                    {GenresEnum.RPG, 10 },
+                    {GenresEnum.Simulators, 4 },
+                    {GenresEnum.Strategy, 3 }
+                }),
+            }),
+            new QuestionModel("Is soup a cereal", "", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 10 },
+                    {GenresEnum.RPG, 5 },
+                    {GenresEnum.Simulators, 10 },
+                    {GenresEnum.Strategy, 0 }
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 20 },
+                    {GenresEnum.RPG, 10 },
+                    {GenresEnum.Simulators, 15 },
+                    {GenresEnum.Strategy, 0 }
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 50 },
+                    {GenresEnum.RPG, 60 },
+                    {GenresEnum.Simulators, 5 },
+                    {GenresEnum.Strategy, 1 }
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<GenresEnum,int>
+                {
+                    {GenresEnum.FirstPersonShooters, 20 },
+                    {GenresEnum.RPG, 10 },
+                    {GenresEnum.Simulators, 4 },
+                    {GenresEnum.Strategy, 3 }
                 }),
             }),
         };
