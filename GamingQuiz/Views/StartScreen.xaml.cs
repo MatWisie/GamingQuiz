7472c6591be6931006eb8be61adf0d69ab59188a
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-08 13:13:47 +0200

    Added StartScreen with Start button

---
+++ GamingQuiz/Views/StartScreen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GamingQuiz.Views
+{
+    /// <summary>
+    /// Interaction logic for StartScreen.xaml
+    /// </summary>
+    public partial class StartScreen : UserControl
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.StartNewQuiz();
+            }
+        }
+    }
+}
