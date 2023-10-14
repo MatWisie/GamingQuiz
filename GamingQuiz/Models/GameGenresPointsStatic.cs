9d5d8cde192258be77370802ae7849628c4b4a3f
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-14 12:29:49 +0200

    Modified GameGenresPointsStatic Dictionary. From now on genre description is in this Dictionary

--- GamingQuiz/Models/GameGenresPointsStatic.cs
+++ GamingQuiz/Models/GameGenresPointsStatic.cs
@@ -4,13 +4,13 @@ namespace GamingQuiz.Models
 {
     public static class GameGenresPointsStatic
     {
-        public static Dictionary<string, int> gameGenresPoints { get; set; } =
-            new Dictionary<string, int>()
+        public static Dictionary<string, (int, string)> gameGenresPoints { get; set; } =
+            new Dictionary<string, (int, string)>()
             {
-                {"Strategy", 0 },
-                {"First Person Shooters", 0 },
-                {"Simulators", 0 },
-                {"RPG", 0 }
+                {"Strategy", (0, "You love thinking etc") },
+                {"First Person Shooters", (0,"You love shooting etc") },
+                {"Simulators", (0,"You love working in gaming wow etc") },
+                {"RPG", (0,"You love fantasy") }
             };
     }
 }
