09ffbfce5bc5e2285f39db530b6a6221234e0c86
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-17 15:48:42 +0200

    Added more questions

--- GamingQuiz/MainWindow.xaml.cs
+++ GamingQuiz/MainWindow.xaml.cs
@@ -24,66 +24,257 @@ namespace GamingQuiz
 
         private List<QuestionModel> questions = new List<QuestionModel>()
         {
-            new QuestionModel("Is cereal a soup", "", new AnswerModel[]
+            new QuestionModel("Do you like being the leader", "https://d19d5sz0wkl0lu.cloudfront.net/dims4/default/c5dc383/2147483647/resize/800x%3E/quality/90/?url=https%3A%2F%2Fatd-brightspot.s3.amazonaws.com%2F082316-leadership.jpg", new AnswerModel[]
             {
                 new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 10 },
-                    {"RPG", 5 },
-                    {"Simulators", 10 },
-                    {"Strategy", 0 }
+                    {"Strategy", 5 },
                 }),
                 new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 0 },
-                    {"RPG", 5 },
-                    {"Simulators", 1 },
-                    {"Strategy", 0 }
+                    {"Strategy", 10 },
+                    {"Role-Playing", 5 }
                 }),
                 new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 20 },
-                    {"RPG", 40 },
-                    {"Simulators", 10 },
-                    {"Strategy", 2 }
+                    {"Fighting", 2 },
+                    {"Puzzle", 2 },
+                    {"Adventure", 2 },
                 }),
                 new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 2 },
-                    {"RPG", 2 },
-                    {"Simulators", 3 },
-                    {"Strategy", 1 }
+                    {"Fighting", 2 }
                 }),
             }),
-            new QuestionModel("Is soup a cereal", "", new AnswerModel[]
+            new QuestionModel("Do you like to make decisions quickly", "https://bmmagazine.co.uk/wp-content/uploads/2012/10/decisionmakingskills.jpg", new AnswerModel[]
             {
                 new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 10 },
-                    {"RPG", 5 },
-                    {"Simulators", 10 },
-                    {"Strategy", 0 }
+                    {"Action", 5 },
+                    {"Shooter", 5 }
                 }),
                 new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 0 },
-                    {"RPG", 52 },
-                    {"Simulators", 10 },
-                    {"Strategy", 1 }
+                    {"Action", 10 },
+                    {"Shooter", 10 },
+                    {"Racing", 5 }
                 }),
                 new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 10 },
-                    {"RPG", 5 },
-                    {"Simulators", 10 },
-                    {"Strategy", 0 }
+                    {"Strategy", 5 },
+                    {"Puzzle", 10 },
+                    {"Adventure", 5 },
+
                 }),
                 new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
                 {
-                    {"First Person Shooters", 22 },
-                    {"RPG", 33 },
-                    {"Simulators", 11 },
-                    {"Strategy", 44 }
+                    {"Strategy", 2 },
+                    {"Puzzle", 5 },
+                    {"Adventure", 2 },
+                }),
+            }),
+            new QuestionModel("Do you like fast-paced action", "https://i0.wp.com/thetechhacker.com/wp-content/uploads/2017/06/Best-Action-Games-for-PC.jpg?fit=1366%2C768&ssl=1", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
+                {
+                    {"Action", 5 },
+                    {"Shooter", 5 },
+                    {"Racing", 5 }
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
+                {
+                    {"Action", 10 },
+                    {"Shooter", 10 },
+                    {"Racing", 10 }
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
+                {
+                    {"Strategy", 5 },
+                    {"Puzzle", 10 },
+                    {"Adventure", 5 },
+                    {"Simulation", 5 },
+
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
+                {
+                    {"Strategy", 2 },
+                    {"Puzzle", 5 },
+                    {"Adventure", 2 },
+                    {"Simulation", 2 },
+                }),
+            }),
+            new QuestionModel("Do you like having to use your reflexes to come out on top", "https://prod.assets.earlygamecdn.com/images/awp-aim.png?transform=article_webp&x=0.5&y=0.5", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
+                {
+                    {"Action", 5 },
+                    {"Shooter", 5 },
+                    {"Racing", 5 },
+                    {"Fighting", 5 }
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
+                {
+                    {"Action", 10 },
+                    {"Shooter", 10 },
+                    {"Racing", 10 },
+                    {"Fighting", 10 }
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
+                {
+                    {"Strategy", 5 },
+                    {"Puzzle", 10 },
+                    {"Adventure", 5 },
+                    {"Simulation", 5 },
+                    {"Role-Playing", 10 },
+
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
+                {
+                    {"Strategy", 2 },
+                    {"Puzzle", 5 },
+                    {"Adventure", 2 },
+                    {"Simulation", 2 },
+                    {"Role-Playing", 5 },
+                }),
+            }),
+            new QuestionModel("Do you enjoy being immersed in the game's world", "https://cdn3.whatculture.com/images/2016/11/e7e270241a7e7398-1200x675.jpg", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
+                {
+                    {"Role-Playing", 10 },
+                    {"Simulation", 5 },
+                    {"Adventure", 2 },
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
+                {
+                    {"Role-Playing", 15 },
+                    {"Simulation", 10 },
+                    {"Adventure", 5 },
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
+                {
+                    {"Racing", 10 },
+                    {"Fighting", 10 }
+
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
+                {
+                    {"Racing", 5 },
+                    {"Fighting", 5 }
+                }),
+            }),
+            new QuestionModel("Do you like good stories", "https://i.redd.it/35olgv0d5q901.png", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
+                {
+                    {"Role-Playing", 10 },
+                    {"Adventure", 2 },
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
+                {
+                    {"Role-Playing", 15 },
+                    {"Adventure", 5 },
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
+                {
+                    {"Racing", 10 },
+                    {"Fighting", 10 },
+                    {"Action", 5 },
+                    {"Platformers", 5 }
+
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
+                {
+                    {"Racing", 5 },
+                    {"Fighting", 5 },
+                    {"Action", 2 },
+                    {"Platformers", 2 }
+                }),
+            }),
+            new QuestionModel("Are you competetive", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/LGD_Gaming_at_the_2015_LPL_Summer_Finals.jpg/1200px-LGD_Gaming_at_the_2015_LPL_Summer_Finals.jpg", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
+                {
+                    {"Shooter", 5 },
+                    {"Racing", 5 },
+                    {"Fighting", 5 },
+                    {"Strategy", 2 },
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
+                {
+                    {"Shooter", 10 },
+                    {"Racing", 10 },
+                    {"Fighting", 10 },
+                    {"Strategy", 5 },
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
+                {
+                    {"Adventure", 10 },
+                    {"Platformers", 15 },
+                    {"Puzzle", 15 },
+                    {"Role-Playing", 10 },
+                    {"Simulation", 10 }
+
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
+                {
+                    {"Adventure", 5 },
+                    {"Platformers", 10 },
+                    {"Puzzle", 10 },
+                    {"Role-Playing", 5 },
+                    {"Simulation", 5 }
+                }),
+            }),
+            new QuestionModel("Do you enjoy solving puzzles", "https://robertheaton.com/images/the-witness-puzzle.jpg", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
+                {
+                    {"Puzzle", 10 },
+                    {"Adventure", 5 },
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
+                {
+                    {"Puzzle", 15 },
+                    {"Adventure", 10 },
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
+                {
+                    {"Simulation", 10 },
+                    {"Shooter", 10 },
+                    {"Racing", 10 },
+                    {"Action", 10 },
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
+                {
+                    {"Simulation", 5 },
+                    {"Shooter", 5 },
+                    {"Racing", 5 },
+                    {"Action", 5 },
+                }),
+            }),
+            new QuestionModel("Do you enjoy exploring new environments", "https://cdn.mos.cms.futurecdn.net/GTvQDMxXZ89cgMShEMETGg.jpg", new AnswerModel[]
+            {
+                new AnswerModel(AnswersEnum.IThinkSo, new Dictionary<string,int>
+                {
+                    {"Role-Playing", 10 },
+                    {"Adventure", 5 },
+                }),
+                new AnswerModel(AnswersEnum.Yes, new Dictionary<string,int>
+                {
+                    {"Role-Playing", 15 },
+                    {"Adventure", 5 },
+                }),
+                new AnswerModel(AnswersEnum.No, new Dictionary<string,int>
+                {
+                    {"Shooter", 5 },
+                    {"Racing", 5 },
+                    {"Fighting", 5 },
+                }),
+                new AnswerModel(AnswersEnum.IDontThinkSo, new Dictionary<string,int>
+                {
+                    {"Shooter", 2 },
+                    {"Racing", 2 },
+                    {"Fighting", 2 },
                 }),
             }),
         };
