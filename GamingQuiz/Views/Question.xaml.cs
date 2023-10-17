18ae58ba52df9347b4bc7d96f4fd154ce15a1a19
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-17 15:48:12 +0200

    Linked questionimage with question

--- GamingQuiz/Views/Question.xaml.cs
+++ GamingQuiz/Views/Question.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using GamingQuiz.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
 namespace GamingQuiz.Views
 {
     /// <summary>
@@ -16,6 +19,7 @@ namespace GamingQuiz.Views
             InitializeComponent();
             this.question = question;
             QuestionTextBlock.Text = question.questionText;
+            QuestionImage.Source = new BitmapImage(new Uri(question.questionImagePath, UriKind.Absolute));
         }
 
         private AnswersEnum selectedAnswer;
@@ -35,7 +39,7 @@ namespace GamingQuiz.Views
                 AnswerModel answer = question.answers.FirstOrDefault(e => e.Name == selectedAnswer);
                 foreach (KeyValuePair<string, int> points in answer.PointsToCategories)
                 {
-                    var newvalue = (GameGenresPointsStatic.gameGenresPoints[points.Key].Item1 + points.Value, GameGenresPointsStatic.gameGenresPoints[points.Key].Item2);
+                    var newvalue = (GameGenresPointsStatic.gameGenresPoints[points.Key].Item1 + points.Value, GameGenresPointsStatic.gameGenresPoints[points.Key].Item2, GameGenresPointsStatic.gameGenresPoints[points.Key].Item3);
                     GameGenresPointsStatic.gameGenresPoints[points.Key] = newvalue;
                 }
 
